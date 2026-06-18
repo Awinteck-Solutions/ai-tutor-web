27d22b42f393a93369d8fa2309e531192d0a172b
Samjay Awinteck <basampana@st.ug.edu.gh>
2026-06-18 12:09:55 +0000

    email service added

--- src/constants/api.constant.jsx
+++ src/constants/api.constant.jsx
@@ -1,2 +1,2 @@
-export const BASEURL = import.meta.env.VITE_API_URL || 'http://localhost:7072';
-// export const BASEURL = import.meta.env.VITE_API_URL || 'https://service.gomlak.com';
+// export const BASEURL = import.meta.env.VITE_API_URL || 'http://localhost:7072';
+export const BASEURL = import.meta.env.VITE_API_URL || 'https://service.gomlak.com';
